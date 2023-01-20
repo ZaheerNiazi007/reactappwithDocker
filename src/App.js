9a6f1da1cf66627b27f67351bf05ca80217f2116
ZaheerNiazi <zaheerniazi1996@gmail.com>
2023-01-20 19:09:16 +0500

    usereducer with api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import Products from "./Products";
 
 export default function App() {
-  const [btcData, setBtcData] = useState({});
+  // const [btcData, setBtcData] = useState({});
 
-  const fetchData = () => {
-    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
-      .then((response) => response.json())
-      .then((jsonData) => setBtcData(jsonData.bpi.USD))
-      .catch((error) => console.log(error));
-  };
+  // const fetchData = () => {
+  //   fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
+  //     .then((response) => response.json())
+  //     .then((jsonData) => setBtcData(jsonData.bpi.USD))
+  //     .catch((error) => console.log(error));
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <>
-      <h1>Current BTC/USD data</h1>
-      <p>Code: {btcData.code}</p>
+      <Products />
+      {/* <h1>Current BTC/USD data</h1> */}
+      {/* <p>Code: {btcData.code}</p>
       <p>Symbol: {btcData.symbol}</p>
       <p>Rate: {btcData.rate}</p>
       <p>Description: {btcData.description}</p>
-      <p>Rate Float: {btcData.rate_float}</p>
+      <p>Rate Float: {btcData.rate_float}</p> */}
     </>
   );
 }
